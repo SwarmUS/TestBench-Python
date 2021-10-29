4f2066a402d387a98458caa91c2622047c09e96c
Charles Quesnel <quesnel.charles@gmail.com>
2021-10-29 11:01:34 -0400

    Fix jig that could stay stuck

--- src/main.py
+++ src/main.py
@@ -13,9 +13,10 @@ from src.turning_station.TurningStation import TurningStation, tickToAngle
 # To use ethernet, you must have a static IP of 192.168.1.101 on submask 255.255.255.0
 USE_ETHERNET = False
 distance = '_0p22_5m' # distance btw BB and distance between HB
+num_frames = 100
 
 if not USE_ETHERNET:
-    hb_stream = UsbStream('/dev/ttyACM2')
+    hb_stream = UsbStream('/dev/ttyACM1')
 else:
     hb_stream = EthernetStream(55551)
     hb_stream.wait_connection()
@@ -27,7 +28,7 @@ hb = HiveBoard(hb_stream, log=False)
 accumulated_data = []
 
 hb.greet()
-hb.set_num_angle_frames(100)
+
 
 # TODO: Add way to turn until position is 0
 sleep(2) # arduino needs to reboot after uart initialisation
@@ -36,11 +37,19 @@ testbench.resetPosition()
 if not os.path.exists('data'):
     os.makedirs('data')
 stepSize = 10
+
 for ticks in tqdm(range(0, 2050 + stepSize, stepSize)):
     testbench.goToTick(ticks)
+    posTick = int(testbench.getPosition())
+
+    hb.set_num_angle_frames(num_frames)
     data = hb.read_angle_data()
 
-    posTick = int(testbench.getPosition())
+    # Ensure we receive all requested data as HB could return less
+    while len(data) < num_frames:
+        hb.set_num_angle_frames(num_frames - len(data))
+        data += hb.read_angle_data()
+
     for frame in data:
         frame['Encoder Tick'] = posTick
         frame['Angle'] = tickToAngle(posTick)
