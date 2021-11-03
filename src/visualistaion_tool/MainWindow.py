232a17f2623683b1eba10665e743055c4ff13077
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-11-03 17:39:10 -0400

    Added stuff to main window as example

--- src/visualistaion_tool/MainWindow.py
+++ src/visualistaion_tool/MainWindow.py
@@ -6,4 +6,6 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("SwarmUS Interlocalisation Visualisation Tool")
+        # Call functions to add ui elements here
+        self.label = QLabel("Ta mere", self)
         self.setGeometry(0, 0, 500, 300)
