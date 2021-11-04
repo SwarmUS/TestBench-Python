4bfc73527f2dd9de0b3054a593152f69e015c61d
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-11-04 13:17:00 -0400

    Added groundwork for setting target agent id

--- src/visualistaion_tool/MainWindow.py
+++ src/visualistaion_tool/MainWindow.py
@@ -3,7 +3,7 @@ import time
 
 from PyQt5.QtWidgets import *
 from Graph2D import Graph2D
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, pyqtSlot
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import numpy as np
@@ -14,6 +14,7 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.target_agent_id = None
 
         #Qt shenaninigans
         self.setWindowTitle("SwarmUS Interlocalisation Visualisation Tool")
@@ -21,6 +22,7 @@ class MainWindow(QMainWindow):
         self.main_layout = QVBoxLayout()
         self.container.setLayout(self.main_layout)
         self.setCentralWidget(self.container)
+
 
         # Create UI elements here
         self.create_2d_graph_area()
@@ -32,7 +34,38 @@ class MainWindow(QMainWindow):
     def create_menu_bar(self):
         menu_bar = self.menuBar()
         config_menu = QMenu("&Configuration", self)
+        target_agent_menu = QMenu("Target Agent", self)
+        self.checkable_agents = []
+        for i in range(1, 7):
+            agent = QAction(f"Agent {i}", self, checkable=True)
+            target_agent_menu.addAction(agent)
+            self.checkable_agents.append(agent)
+            agent.triggered.connect(self.update_target_agent_from_config)
+        config_menu.addMenu(target_agent_menu)
         menu_bar.addMenu(config_menu)
+
+    @pyqtSlot(bool)
+    def update_target_agent_from_config(self, checked: bool):
+        if checked:
+            checked_agents = [agent for agent in self.checkable_agents if agent.isChecked()]
+            new_agent_id = 0
+            for agent in checked_agents:
+                if self.checkable_agents.index(agent) + 1 == self.target_agent_id:
+                    agent.setChecked(False)
+                else:
+                    new_agent_id = self.checkable_agents.index(agent) + 1
+            self.target_agent_id = new_agent_id
+            print(f"New target agent is {self.target_agent_id} based on configuration set")
+
+    @pyqtSlot(int)
+    def update_target_agent_from_greeting(self, agent_id):
+        self.target_agent_id = agent_id
+        for agent in self.checkable_agents:
+            agent.setChecked(False)
+        self.checkable_agents[agent_id-1].setChecked(True)
+        print(f"New target agent is {self.target_agent_id} based on greeting received")
+
+
 
     def create_2d_graph_area(self):
         self.graphWidget = Graph2D(self)
