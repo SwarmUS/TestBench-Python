54450d85a7ca5aca0d2eba810172784086dd800a
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-11-03 20:49:36 -0400

    Can now plot and update random points in plot area

---
+++ src/visualistaion_tool/Graph2D.py
@@ -0,0 +1,44 @@
+from PyQt5 import QtWidgets
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import numpy as np
+
+
+class Graph2D(QtWidgets.QWidget):
+
+    def __init__(self, parent):
+        super(Graph2D, self).__init__(parent)
+        self.layout = QtWidgets.QGridLayout()
+        self.setLayout(self.layout)
+
+        self.graphWidget = pg.plot()
+        self.scatter = pg.ScatterPlotItem(size=10)
+
+        n = 10
+        pos = np.random.normal(size=(2, n), scale=1e-5)
+
+        # creating spots using the random position
+        spots_1 = [{'pos': pos[:, i], 'data': i}
+                 for i in range(n)] + [{'pos': [0, 0], 'data': 1}]
+
+        self.update_points(pos.transpose())
+        #self.scatter.addPoints(spots_1)
+        self.graphWidget.addItem(self.scatter)
+        self.layout.addWidget(self.graphWidget)
+
+    def update_points(self, points: list):
+        self.clear_points()
+        i = 0
+        spots = []
+        for i in range(len(points)):
+            spot = {'pos': points[i],
+                    'pen': {'color': 'w', 'width': 1},
+                    'brush': pg.intColor(i * 10, 100)}
+            spots.append(spot)
+            i += 1
+        self.scatter.setData(spots)
+        self.graphWidget.addItem(self.scatter)
+
+    def clear_points(self):
+        self.graphWidget.removeItem(self.scatter)
+        self.scatter.clear()
--- src/visualistaion_tool/MainWindow.py
+++ src/visualistaion_tool/MainWindow.py
@@ -1,11 +1,47 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+import threading
+import time
+
+from PyQt5.QtWidgets import *
+from Graph2D import Graph2D
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import numpy as np
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
         self.setWindowTitle("SwarmUS Interlocalisation Visualisation Tool")
+
+
         # Call functions to add ui elements here
-        self.label = QLabel("Ta mere", self)
-        self.setGeometry(0, 0, 500, 300)
+        self.graphWidget = Graph2D(self)
+        self.setCentralWidget(self.graphWidget)
+        self.create_menu_bar()
+
+    def create_menu_bar(self):
+        menu_bar = self.menuBar()
+        config_menu = QMenu("&Configuration", self)
+        menu_bar.addMenu(config_menu)
+
+        clear_action = QAction("Clear", self)
+        clear_action.setStatusTip("Update graph with test data")
+        clear_action.triggered.connect(self.graphWidget.clear_points)
+        menu_bar.addAction(clear_action)
+
+        add_action = QAction("Add data", self)
+        add_action.setStatusTip("Update graph with test data")
+        add_action.triggered.connect(self.run)
+        menu_bar.addAction(add_action)
+
+
+
+    def run(self, e):
+        n = 10
+        pos = np.random.normal(size=(2, n), scale=1e-5)
+        self.graphWidget.update_points(pos.transpose())
+
+
+
+
