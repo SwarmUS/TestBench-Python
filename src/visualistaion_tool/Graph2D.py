69e6709e836b8ebbe1f5c323aa6f636b26738703
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-11-07 19:36:54 -0500

    Added legend to graph

--- src/visualistaion_tool/Graph2D.py
+++ src/visualistaion_tool/Graph2D.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot
-from pyqtgraph import PlotWidget, plot, GridItem
+from pyqtgraph import PlotWidget, plot, GridItem, LegendItem
 import pyqtgraph as pg
 
 COLOR_OFFSET = 15
@@ -12,13 +12,11 @@ class Graph2D(QtWidgets.QWidget):
         super(Graph2D, self).__init__(parent)
         self.layout = QtWidgets.QGridLayout()
         self.setLayout(self.layout)
-        self.points = {}
+        self.scatters = {}
 
         self.create_2d_plot()
         self.create_grid()
         self.create_scatter_element()
-
-
 
     def create_2d_plot(self):
         self.graphWidget = pg.plot()
@@ -28,6 +26,7 @@ class Graph2D(QtWidgets.QWidget):
         self.graphWidget.setXRange(-7, 7)
         self.graphWidget.setYRange(-7, 7)
         self.graphWidget.setMouseEnabled(x=False, y=False)
+        self.graphWidget.addLegend()
         self.layout.addWidget(self.graphWidget)
 
     def create_grid(self):
@@ -35,7 +34,6 @@ class Graph2D(QtWidgets.QWidget):
         self.graphWidget.addItem(self.grid)
 
     def create_scatter_element(self):
-        self.scatter = pg.ScatterPlotItem()
         self.base = pg.ScatterPlotItem()
         base_symbol = {'pos': [0, 0],
                        'pen': {'color': 'w', 'width': 1},
@@ -44,16 +42,20 @@ class Graph2D(QtWidgets.QWidget):
                        'size': 30}
         self.base.addPoints([base_symbol])
         self.graphWidget.addItem(self.base)
-        self.graphWidget.addItem(self.scatter)
-
 
     @pyqtSlot(int, float, float)
     def update_point(self, neighbor_id: int, x: float, y: float):
-        self.points[neighbor_id] = {'pos': [x, y],
-                                    'pen': {'color': 'w', 'width': 1},
-                                    # 100 is the number int values for the color spectrum,
-                                    # COLOR_OFFSET is the offset to apply to have 6 different values for points
-                                    'brush': pg.intColor(neighbor_id * COLOR_OFFSET, 100),
-                                    'size': 20}
-        self.scatter.clear()
-        self.scatter.setData(list(self.points.values()))
+        if neighbor_id not in self.scatters.keys():
+            self.scatters.update({neighbor_id: {"scatter": pg.ScatterPlotItem(name=f"agent {neighbor_id}",
+                                                                              brush=pg.intColor(neighbor_id * COLOR_OFFSET, 100)),
+                                                "spot": {}}})
+            self.graphWidget.addItem(self.scatters[neighbor_id]["scatter"])
+
+        self.scatters[neighbor_id]["spot"] = {'pos': [x, y],
+                                              'pen': {'color': 'w', 'width': 1},
+                                              # 100 is the number int values for the color spectrum,
+                                              # COLOR_OFFSET is the offset to apply to have 6 different values for points
+                                              'brush': pg.intColor(neighbor_id * COLOR_OFFSET, 100),
+                                              'size': 20}
+        self.scatters[neighbor_id]["scatter"].setData(spots=[self.scatters[neighbor_id]["spot"]],
+                                                      name=f"Agent {neighbor_id}")
